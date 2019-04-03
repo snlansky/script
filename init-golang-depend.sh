2ae8a57e8b251c138f73f3bfa724299d338e82e9
snlan@live.cn <snlan@live.cn>
2019-04-03 10:24:00 +0800

    update

---
+++ init-golang-depend.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+go get -u golang.org/x/tools/cmd/goimports
+go get -u golang.org/x/tools/cmd/vet
+go get -u golang.org/x/tools/cmd/oracle
+go get -u golang.org/x/tools/cmd/godoc
+go get -u github.com/golang/lint/golint
+go get -u github.com/nsf/gocode
