bd6ab4177b2457ae65fa15e68455a5de15061a1d
snlan@live.cn <snlan@live.cn>
2019-04-06 13:37:23 +0800

    add ignore

--- setup_env.sh
+++ setup_env.sh
@@ -2,12 +2,12 @@
 
 echo -e "Backuping existing apt configuration"
 timestr=$(date +%Y%m%d%H%M)
-tar -zcPf /etc/apt.$timestr.tar.gz /etc/apt
+sudo tar -zcPf /etc/apt.$timestr.tar.gz /etc/apt
 
-#aptKeys=(
-#  https://download.docker.com/linux/ubuntu/gpg # docker-ce
-#)
-#for k in ${aptKeys[@]}; do echo "Adding apt key: ${k}"; curl -fsSL $k | sudo apt-key add -; done
+aptKeys=(
+  https://download.docker.com/linux/ubuntu/gpg # docker-ce
+)
+for k in ${aptKeys[@]}; do echo "Adding apt key: ${k}"; curl -fsSL $k | sudo apt-key add -; done
 
 aptRepositories=(
   ppa:jonathonf/vim # vim
@@ -19,18 +19,18 @@ aptRepositories=(
 )
 for ((i = 0; i < ${#aptRepositories[@]}; i++));
 do
-    echo "Adding apt repository: ${aptRepositories[$i]}"
-    add-apt-repository -y -n "${aptRepositories[$i]}"
+  echo "Adding apt repository: ${aptRepositories[$i]}"
+  sudo add-apt-repository -y -n "${aptRepositories[$i]}"
 done
 
 
-apt update
-apt install -y build-essential libncurses-dev git make curl unzip gcc g++ libtool telnet wget tar unzip rar unrar ack-grep tmux zsh \
-                binutils bison apt-transport-https ca-certificates software-properties-common gdebi net-tools \
-                sysstat nmon htop atop iotop iftop nethogs ethtool nicstat dstat vnstat pstack strace colordiff \
-                tmux zsh autojump ack-grep vim vim-gtk exuberant-ctags tree i3 suckless-tools flameshot ansible fcitx \
-                mycli mongodb-clients mongo-tools redis-tools python-software-properties virtualbox emacs26 vlc \
-                docker.io
+sudo apt update
+sudo apt install -y build-essential libncurses-dev git make curl unzip gcc g++ libtool telnet wget tar unzip rar unrar ack-grep tmux zsh \
+                    binutils bison apt-transport-https ca-certificates software-properties-common gdebi net-tools \
+                    sysstat nmon htop atop iotop iftop nethogs ethtool nicstat dstat vnstat pstack strace colordiff \
+                    tmux zsh autojump ack-grep vim vim-gtk exuberant-ctags tree i3 suckless-tools flameshot ansible fcitx \
+                    mycli mongodb-clients mongo-tools redis-tools python-software-properties virtualbox emacs26 vlc \
+                    docker-ce
 
 
 # ----------------------------------------------------------------
@@ -45,18 +45,67 @@ do
   filename=$aptCache/$(basename $i)
   if [ ! -f $filename ]; then
     echo -e "\nInstalling $i"
-    curl -L -o$filename $i
-    gdebi -n $filename
+    sudo curl -L -o$filename $i
+    sudo gdebi -n $filename
   fi
 done
 
 # ----------------------------------------------------------------
-# Install docker
-usermod -aG docker $(whoami)
-# install docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
+# install tmux
+if [[ `tmux -V` != "tmux 2.8" ]]; then
+  curl -L https://github.com/tmux/tmux/releases/download/2.8/tmux-2.8.tar.gz -o tmux-2.8.tar.gz
+  tar -zxvf tmux-2.8.tar.gz
+  cd tmux-2.8
+  sudo apt install libevent-dev ncurses-dev
+  sudo ./configure
+  sudo make
+  sudo make install
+  cd .. && sudo rm -rf tmux-2.8 tmux-2.8.tar.gz
+fi
+
+
+# ----------------------------------------------------------------
+# https://github.com/robbyrussell/oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  echo -e "\nInstalling oh-my-zsh..."
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  chsh -s /bin/zsh
+fi
+
+# ----------------------------------------------------------------
+# https://github.com/junegunn/fzf
+if [ ! -d $HOME/.fzf ]; then
+  echo -e "\nInstalling fzf..."
+  git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
+fi
+
+# ----------------------------------------------------------------
+# https://github.com/creationix/nvm
+if [ ! -d $HOME/.nvm ]; then
+  echo -e "\nInstalling nvm..."
+  curl -L https://github.com/creationix/nvm/raw/v0.33.11/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  nvm install 8
+  nvm use 8
+  nvm alias default 8
+
+  npm install -g http-server cleaver
+fi
+
+
+# ----------------------------------------------------------------
+# Add user account to the docker group
+sudo usermod -aG docker $(whoami)
+
+# https://github.com/docker/compose
+(command -v docker-compose >/dev/null 2>&1) || {
+  echo -e "\nInstalling docker-compose..."
+  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+}
 
 
 # ----------------------------------------------------------------
@@ -69,26 +118,20 @@ export GOPATH="/opt/gopath"
 export GOROOT="/opt/go"
 PATH=$GOROOT/bin:$GOPATH/bin:$PATH
 
-cat <<EOF >/etc/profile.d/goroot.sh
+sudo cat <<EOF >/etc/profile.d/goroot.sh
 export GOROOT=$GOROOT
 export GOPATH=$GOPATH
 export PATH=\$PATH:$GOROOT/bin:$GOPATH/bin
 EOF
 
-
-rm -rf $GOROOT
-mkdir -p $GOROOT
-curl -sL $GO_URL | (cd $GOROOT && tar --strip-components 1 -xz)
-
-
-# ----------------------------------------------------------------
-# Install npm
-apt install npm
-npm install -g http-server cleaver
+sudo rm -rf $GOROOT
+sudo mkdir -p $GOROOT
+sudo mkdir -p $GOPATH
+sudo curl -sL $GO_URL | (cd $GOROOT && tar --strip-components 1 -xz)
 
 
 # ----------------------------------------------------------------
-# Install java
+echo "Install java"
 # must init ppa:webupd8team/java repositore
 apt install oracle-java8-installer
 java -version
@@ -99,23 +142,6 @@ apt install maven
 # Install rust
 curl https://sh.rustup.rs -sSf | bash
 
-
-# https://github.com/robbyrussell/oh-my-zsh
 # ----------------------------------------------------------------
-if [[ ! -d $HOME/.oh-my-zsh ]]; then
-  echo -e "\nInstalling oh-my-zsh..."
-  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-  chsh -s /bin/zsh
-fi
-
-
-# ----------------------------------------------------------------
-# https://github.com/junegunn/fzf
-if [[ ! -d $HOME/.fzf ]]; then
-  echo -e "\nInstalling fzf..."
-  git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install
-fi
-
-
 # Remove automatically all unused packages
-apt autoremove -y
+sudo apt autoremove -y
