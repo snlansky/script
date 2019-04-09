48de1187fd0f6bac661120d7e52212911d4bd791
snlan@live.cn <snlan@live.cn>
2019-04-09 18:52:41 +0800

    add go depdent

--- init-golang-depend.sh
+++ init-golang-depend.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-go get -u golang.org/x/tools/cmd/godoc
-go get -u golang.org/x/lint/golint
-go get -u github.com/nsf/gocode
 go get -u github.com/dougm/goflymake
 go get -u github.com/rogpeppe/godef
-# old name oracle
-go get -u golang.org/x/tools/cmd/guru
 go get -u github.com/kisielk/errcheck
+go get -u github.com/nsf/gocode
+
+go get -u github.com/mdempsky/gocode # github.com/nsf/gocode
+go get -u golang.org/x/lint/golint
+go get -u golang.org/x/tools/cmd/guru
+go get -u golang.org/x/tools/cmd/gorename
+go get -u golang.org/x/tools/cmd/gotype
+go get -u golang.org/x/tools/cmd/godoc
+go get -u github.com/derekparker/delve/cmd/dlv
+go get -u github.com/josharian/impl
+go get -u github.com/cweill/gotests/...
+go get -u github.com/fatih/gomodifytags
+go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+
 gocode set propose-builtins true
