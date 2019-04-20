cb69fc89f4f74d6093fcc45f477aa7abb575df65
snlan <snlan@live.cn>
2019-04-20 11:13:29 +0800

    Update init-rust-depend.sh

--- init-rust-depend.sh
+++ init-rust-depend.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cargo install protobuf-codegen
+cargo install grpcio-compiler
 cargo install racer
 cargo install --force rustfmt
 cargo install --force rls
@@ -7,3 +9,4 @@ cargo install --force rls
 rustup component add rls-preview
 rustup component add rust-analysis
 rustup component add rust-src
+rustup component add clippy-preview
