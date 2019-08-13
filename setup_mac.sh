d12b5b56d45bd5f00ab5b87b54f71e1792e6beeb
snlan <snlan@live.cn>
2019-08-13 19:57:14 +0800

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -5,3 +5,11 @@
 sudo brew install git
 brew install tmux
 brew install zsh fzf 
+
+# ----------------------------------------------------------------
+# https://github.com/robbyrussell/oh-my-zsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  echo -e "\nInstalling oh-my-zsh..."
+  curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+  chsh -s /bin/zsh
+fi
