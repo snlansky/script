bdc88a695b1d4cf3c5f8dcabd9adc55bbf024552
snlan <snlan@live.cn>
2020-01-03 11:01:14 +0800

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -22,3 +22,8 @@ usermod -a -G docker snlan
   sudo chmod +x /usr/local/bin/docker-compose
 }
 
+# install k8s
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl version
