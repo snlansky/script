6d36d0e9642c4c0f682f6e7074c3221c9a90f0a3
snlan <snlan@live.cn>
2020-01-03 17:08:36 +0800

    Update init-docker.sh

--- init-docker.sh
+++ init-docker.sh
@@ -8,13 +8,8 @@ curl https://releases.rancher.com/install-docker/18.09.sh | sh
 
 echo "install docker success!"
 
-
-# https://github.com/docker/compose
-(command -v docker-compose >/dev/null 2>&1) || {
-  echo -e "\nInstalling docker-compose..."
-  sudo curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-}
+# install docker-compose
+sudo apt install docker-compose
 
 # install k8s
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
