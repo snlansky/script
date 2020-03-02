3475f19364d675c3d505e8ec3f6a459395c46256
snlan <snlan@live.cn>
2020-03-02 14:02:15 +0800

    Update setup_manjaro.sh

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# https://manjaro.org/download/official/gnome/
+
 sudo pacman-mirrors -c China
 sudo pacman -Sc
 sudo pacman -Syyu
+
 sudo pacman -S net-tools
-# https://manjaro.org/download/official/gnome/
+
 
