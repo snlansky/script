cf1bf4f18bbe60811fe220a92491a707366fe2ee
snlan <snlan@live.cn>
2020-11-16 18:56:41 +0800

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -27,8 +27,7 @@ sudo apt install -y lsb-release lsb-core net-tools telnet wget git-extras make g
                     mercurial binutils build-essential bison apt-transport-https ca-certificates software-properties-common gdebi \
                     sysstat nmon htop atop iotop iftop nethogs ethtool nicstat dstat vnstat pstack strace colordiff openssh-server \
                     python-pip python3-pip tmux zsh autojump ack-grep silversearcher-ag vim vim-gtk neovim exuberant-ctags suckless-tools \
-                    mycli mongodb-clients mongo-tools redis-tools usb-creator-gtk telegram-desktop transmission flameshot tree \
-                    vlc virtualbox i3 fcitx jq emacs26 kazam exfat-utils libzmq3-dev libssl-dev protobuf-compiler libreadline-dev \
+                    flameshot tree vlc i3 fcitx jq emacs26 exfat-utils libzmq3-dev libssl-dev protobuf-compiler libreadline-dev \
                     fonts-powerline fonts-firacode cloc
 
 
